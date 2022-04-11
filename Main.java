d3c2cae33ea6949297587dfb197b772492491664
Jassherrin <64390744+Jassherrin@users.noreply.github.com>
2022-04-11 15:23:49 +0000

    some stuff

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
 class Main {
   public static void main(String[] args) {
     System.out.println("Hello world!");
-  }
+
+    String name = "Mark Anderson";
+    String stringName = String.format("My name is %s",name);
+    System.out.println(stringName);
+    }
 }
